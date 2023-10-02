70628813f3b47559720fef78320531fb04394cc1
Indah Putri Rahmi Nurilah <131329672+Indahprn@users.noreply.github.com>
2023-10-02 12:45:42 +0700

    Add files via upload

---
+++ profile_arrow_function.js
@@ -0,0 +1,6 @@
+const profile = (nama1, nama2, email) => {
+  console.log("Nama: ", nama1, nama2)
+  console.log("Email: ", email)
+}
+
+profile("Indah", "Putri Rahmi Nurilah", "indahputri010906@gmail.com" )
